0579747f75f3c8c87525d4425c1cc023d2a5bfc0
Mike Calixte <m_calix@encs.concordia.ca>
2019-11-17 08:20:58 -0500

    Feature/react migration (#9)
    * React migration without CSS
    
    * Deleted React-NAtive files
    
    * Update index.js

---
+++ client/react-components/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+//import SignInSide from './components/signin';
+//import Pills from './components/DataVisualization';
+//import PillsWrapper from './components/PillsWrapper';
+/*eslint-disable-next-line*/
+//import { BrowserRouter as Router } from 'react-router-dom';
+import ChallengeCards from './components/ChallengeCards';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+       <ChallengeCards/>
+   
+    </div>
+  );
+}
+
+export default App;
